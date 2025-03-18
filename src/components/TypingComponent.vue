78457bc9643efcccdcfb5580f2a42a2218224ff5
ThePension <nicolas2001@hotmail.ch>
2025-03-18 10:03:02 +0100

    Improve general styling + words processing

--- src/components/TypingComponent.vue
+++ src/components/TypingComponent.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-const words = ref([
+// Word state enum (not typed, correct, incorrect)
+const WORD_STATE = {
+  NOT_TYPED: 0,
+  CORRECT: 1,
+  INCORRECT: 2,
+  TO_TYPE: 3,
+};
+
+const available_words = ref([
   "le",
   "de",
   "un",
@@ -54,10 +62,9 @@ const words = ref([
   "deux",
 ]);
 
-const user_input = ref("");
-const words_box_width = ref(800);
+const words = ref([]);
 
-const word_per_line = ref(0);
+const user_input = ref("");
 
 const words_count = ref(0);
 
@@ -72,82 +79,125 @@ const checkInput = () => {
   // Get the last char of the user input
   const last_char = user_input.value[user_input.value.length - 1];
 
-  // If last char is a space
+  // Get first word from words variable
+  const current_word = words.value[words_count.value].word;
+  const typed_word = user_input.value.trim();
+
+  // If last char is not a space
   if (last_char === " ") {
-    // Get first word from words variable
-    const current_word = words.value[words_count.value];
-    const typed_word = user_input.value.trim();
-
     if (current_word == typed_word) {
-      // Increment char_count by length of typed_word
-      char_count.value += current_word.length;
 
-      console.log("Correct");
+      // Update state of current word to correct
+      words.value[words_count.value].state = WORD_STATE.CORRECT;
     }
+    else {
+      // Update state of current word to incorrect
+      words.value[words_count.value].state = WORD_STATE.INCORRECT;
+    }
+
+    words.value[words_count.value + 1].state = WORD_STATE.TO_TYPE;
 
     // Increment words_count by 1
     words_count.value += 1;
 
-    // Calculate WPM : a word has 5 characters on average
-    wpm.value = Math.round(char_count.value / 5 / (elapsed_time.value / 60));
-
     // Clear user input
     user_input.value = "";
-
-    // If an entire line has been typed
-    if (words_count.value == word_per_line.value) {
-      // Remove the current line from words variable
-      words.value.splice(0, word_per_line.value);
-
-      // Reset words_count
-      words_count.value = 0;
-    }
+  } else if (current_word.startsWith(typed_word)) {
+    // Update state of current word to to_type
+    char_count.value += 1;
   }
+
+  // Calculate WPM : a word has 5 characters on average
+  wpm.value = Math.round(char_count.value / 5.0 / (elapsed_time.value / 60.0));
 };
 
 // If the user press on the escape key
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    // Clear user input
-    user_input.value = "";
+    initialize();
   }
 });
 
-onMounted(() => {
-  // Based on the window size, get the number of words per line
-  word_per_line.value = Math.floor(words_box_width.value / 65);
+const initialize = () => {
+  // Clear user input
+  user_input.value = "";
+  words_count.value = 0;
+  char_count.value = 0;
 
-  console.log(word_per_line.value);
+  // Focus on the input box
+  document.querySelector(".q-input").focus();
+
+  // Initialize words
+  for (let i = 0; i < 60; i++) {
+    words.value[i] = {
+      word: available_words.value[Math.floor(Math.random() * available_words.value.length)],
+      state: WORD_STATE.NOT_TYPED,
+    };
+  }
+
+  // Set the first word to TO_TYPE
+  words.value[0].state = WORD_STATE.TO_TYPE;
+
+  // Stop timer
+  clearInterval(timer.value);
+  elapsed_time.value = 0;
+  wpm.value = 0;
 
   // Start timer
   timer.value = setInterval(() => {
     // Increment elapsed_time by 1
     elapsed_time.value += 1;
   }, 1000);
+}
+
+onMounted(() => {
+  initialize();
 });
 </script>
+
+<style scoped>
+  span { 
+    width: 100%;
+    text-align: center;
+    margin-right: 5px;
+    padding: 5px;
+    font-size: 20px;
+  }
+
+  #input-box {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  #wpm {
+    text-align: center;
+    font-size: 20px;
+    margin-top: 20px;
+  }
+</style>
 
 <template>
   <div>
     <!-- Rectangle box for displaying words, using words variable -->
     <div class="row" style="width: 800px">
-      <div class="col-1" v-for="word in words" :key="word">
-        {{ word }}
+
+      <div class="col" style="word-wrap: break-word;">
+
+          <span v-for="word in words" :key="word" :style="{ 
+            color: word.state === 1 ? 'green' : word.state === 2 ? 'red' : 'black',
+            backgroundColor: word.state === 3 ? 'yellow' : 'white',
+            }">
+              &nbsp;{{ word.word }}&nbsp;
+          </span>
       </div>
     </div>
 
     <!-- Input for user typing input -->
-    <div class="input-box">
-      <input
-        type="text"
-        v-model="user_input"
-        placeholder="Type here..."
-        class="input-box__input"
-        @input="checkInput"
-      />
-
-      <!-- Display WPM -->
-      <div class="input-box__wpm">{{ wpm }} WPM</div>
+    <div id="input-box">
+      <q-input filled v-model="user_input" @update:model-value="checkInput"/>
     </div>
+
+    <!-- Display WPM -->
+    <div id="wpm">{{ wpm }} WPM</div>
   </div>
 </template>
