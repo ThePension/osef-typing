27629f4da99890e306d6c3c74aac6387f50a83fb
ThePension <nicolas2001@hotmail.ch>
2025-03-19 08:09:07 +0100

    Add github repo link

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,10 @@
     <q-header elevated pink color class="bg-pink-8">
       <q-toolbar>
         <q-toolbar-title> Osef Typing</q-toolbar-title>
+
+        <a href="https://github.com/ThePension/osef-typing" target="_blank">
+          <i class="bi bi-github" style="font-size: 30px; color: white; margin-right: 10px"></i>
+        </a>
       </q-toolbar>
     </q-header>
 
