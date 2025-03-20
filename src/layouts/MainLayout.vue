d639beadf683cbe2938cb450159c7868ff6fa99c
ThePension <nicolas2001@hotmail.ch>
2025-03-20 07:05:28 +0100

    Add navbar with routing and lang select

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,32 +6,112 @@ const languageStore = useLanguageStore();
 
 const lang = ref(languageStore.getLanguage());
 
+const drawer = ref(false)
+
+const innerWidth = ref(window.innerWidth)
+
+// Create languages list from the LANGUAGES dictionary
+const languagesList = Object.values(LANGUAGES);
+
+const menuList = [
+  {
+    label: 'Words Typing',
+    route: '/words',
+  },
+  {
+    label: 'Text Typing',
+    route: '/text',
+  },
+]
+
 </script>
 
+<style>
+#header {
+  height: 70px;
+  line-height: 70px;
+}
+
+#title {
+  font-size: 30px;
+  color: white;
+}
+
+.link {
+  cursor: pointer;
+  text-align: center;
+  font-size: 20px;
+  margin: 10px 0;
+}
+</style>
+
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated pink color class="bg-pink-8">
-      <q-toolbar>
-        <q-toolbar-title> Osef Typing</q-toolbar-title>
+  <div class="q-pa-md">
+    <q-layout view="hHh Lpr lff">
+      <q-header id="header" elevated pink color class="bg-pink-8">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
 
-        <q-tabs v-model="lang"
-                class="q-mx-lg"         
-                dense 
-                @update:model-value="languageStore.setLanguage(lang)"
-        >
-          <q-tab v-for="language in LANGUAGES" :name="language" :key="language" :label="language" />
-        </q-tabs>
+          <q-toolbar-title id="title"> Osef Typing</q-toolbar-title>
 
-        <a href="https://github.com/ThePension/osef-typing" target="_blank">
-          <i class="bi bi-github" style="font-size: 30px; color: white; margin-right: 10px"></i>
-        </a>
+          <q-select
+            v-model="lang"
+            dense
+            label-color="orange"
+            class="q-mx-lg"
+            v-if="innerWidth > 500"
+            :options="languagesList"
+            @update:model-value="languageStore.setLanguage(lang)"
+          />
 
-      </q-toolbar>
-      
-    </q-header>
+          <a href="https://github.com/ThePension/osef-typing" target="_blank">
+            <i class="bi bi-github" style="font-size: 30px; color: white; margin-right: 10px"></i>
+          </a>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        </q-toolbar>
+
+        {{  currentRouteName }}
+        
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+      >
+        <q-select
+          v-model="lang"
+          dense
+          label-color="orange"
+          class="q-mx-lg"
+          v-if="innerWidth < 500"
+          :options="languagesList"
+          @update:model-value="languageStore.setLanguage(lang)"
+        />
+
+        <q-scroll-area class="fit">
+          <q-list>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item class="link" clickable v-ripple :active="menuItem.route === $route.path">
+                <q-item-section @click="$router.push(menuItem.route)">
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+
+      </q-drawer>
+
+      <q-page-container>
+        <q-page padding>
+          <router-view />
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
